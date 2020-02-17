814c387d7532843ef9496114b223f741714c094a
Cris Moen <cris.moen@runtosolve.com>
2020-02-17 10:12:21 -0500

    SDOF EOM with rectangular step loading

--- Lecture 7 - arbitrary loadings/SpringMassStep.jl
+++ Lecture 7 - arbitrary loadings/SpringMassStep.jl
@@ -1,8 +1,9 @@
 #define Julia ODE package that we will use as solver
 using OrdinaryDiffEq
+using Dierckx  #this is for interpolating an aribtrary loading
 
 #define constants
-m=3000000    #kg
+m=300    #kg
 g=9.8  #m/sec^2
 Tn=0.5  #seconds
 fn=1/Tn
@@ -10,11 +11,11 @@ fn=1/Tn
 
 k=ωn^2*m    #N/m
 
-ζ=0.00      #viscous damping ratio
+ζ=0.05      #viscous damping ratio
 ccr=2*m*ωn  #critical viscous damping coefficient
 c=ζ*ccr #viscous damping coefficient, units of kg/sec.
 
-μ=0.50  #friction coefficient
+μ=0.00  #friction coefficient
 N=m*g   #normal force
 F=μ*N   #friction force
 
@@ -27,12 +28,44 @@ Tn=1/fn       #period, seconds
 a=0 #N
 ω=0  #radians/sec.
 
+#define arbitrary function
+#in this case a rectangular step function
+
+function RectangularStepLoading(StepMagnitude,StepTimeStart,StepTimeEnd,t)
+
+    if (t>=StepTimeStart) & (t<=StepTimeEnd)
+        fStep=StepMagnitude
+    else
+        fStep=0.0
+    end
+
+    return fStep
+end
+
+StepMagnitude=100.0
+StepTimeStart=0.0
+StepTimeEnd=4.0
+tArb=0:0.001:10
+fArb=StepLoading.(StepMagnitude,StepTimeStart,StepTimeEnd,tArb)
+
+plot(tArb,fArb)
+
+#define abitrary force as a spline for later interpolation
+spl = Spline1D(tArb,fArb)
+
+#check that spline is matching curve
+fArbCheck=spl.(tArb)
+plot!(tArb,fArbCheck)
+
 #write equation of motion
 function SpringMassDamperEOM(ddu,du,u,p,t)
-    m, k, c, a, ω, F  = p
+    m, k, c, a, ω, F, spl  = p
 
     #harmonic forcing function
-    pt=a*sin(ω*t[1])
+    ptForcing=a*sin(ω*t[1])
+
+    #interpolate abitrary force
+    ptArb = spl(t[1])
 
     #friction damping
     if du[1]>0
@@ -41,19 +74,19 @@ function SpringMassDamperEOM(ddu,du,u,p,t)
         SignF=-1
     end
 
-    ddu[1] = -c/m*du[1] - k/m*u[1] + pt/m -F*SignF/m
+    ddu[1] = -c/m*du[1] - k/m*u[1] + ptForcing/m -F*SignF/m + ptArb/m
 end
 
 #solver controls
-dt=0.001 #seconds
+dt=0.0001 #seconds
 totalt=10 #seconds
 
 #define initial conditions
-uo=0.1  #meters
+uo=0.0  #meters
 duo=0.0 #m/sec.
 
 #solve the ODE
-prob = SecondOrderODEProblem(SpringMassDamperEOM, [duo], [uo], (0.,totalt), (m, k, c, a, ω, F))
+prob = SecondOrderODEProblem(SpringMassDamperEOM, [duo], [uo], (0.,totalt), (m, k, c, a, ω, F, spl))
 sol = solve(prob, DPRKN6(),tstops=0:dt:totalt)
 
 #separate out u(t),du(t), and t from solution
